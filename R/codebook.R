0e6c2ea57e22506db00a8001b8ef45925cb4478f
Anh Le <aql3@duke.edu>
2016-07-23 16:49:16 +0700

    merge conflict

--- R/codebook.R
+++ R/codebook.R
@@ -1,17 +1,11 @@
 #' Get codebook information of the variable
 #'
-codebook <- function(x, ...) {
+codebook <- function(x, width = NULL, ...) {
   UseMethod("codebook")
 }
 
-codebook.labelled <- function(x) {
-  label <- attr(x, "label")
-  type <- type_sum(x)
-  cat(paste0("Label: ", label, " Type: ", type))
-}
-
-codebook.numeric <- function(x) {
-  width <- tibble_width(NULL)
+codebook.numeric <- function(x, width = NULL) {
+  width <- tibble_width(width)
 
   padleft <- function(s) format(s, w = width / 3, justify = "right")
   # align <- function(s) format(c(s, "unique values: "), justify = "right")[1]
@@ -27,16 +21,24 @@ codebook.numeric <- function(x) {
   mean <- paste0(padleft("mean: "), format(mean(x, na.rm = TRUE), digits = 3))
   stddev <- paste0(padleft("std. dev.: "), format(sd(x, na.rm = TRUE), digits = 3))
 
-  percentiles1 <- paste0(padleft("percentiles: "),
-                         "10%  ", "25%  ", "50%  ", "75%  ", "90%  ")
-  percentiles2 <- c(padleft(" "),
-                    paste0(format(quantile(x, c(0.1, 0.25, 0.5, 0.75, 0.9)), digits = 3),
-                    "  "))
+  percentile <- format(c("10%", "25%", "50%", "75%", "90%",
+                       format(quantile(x, c(0.1, 0.25, 0.5, 0.75, 0.9)), digits = 3)))
 
-  cat(codebook_header(x))
+  cat(codebook_header(x, width = width))
   cat(type, "\n", "\n", range, "\n", sep = "")
   cat(paste0(unique_values,
-             format(missing, w = width / 3, justify = "right")), "\n", "\n")
-  cat(mean, "\n", stddev, "\n", "\n", percentiles1, "\n", percentiles2, sep = "")
+             format(missing, w = width / 3, justify = "right")), "\n", "\n", sep = "")
+  cat(mean, "\n", stddev, "\n", "\n", sep = "")
+  cat(padleft("percentiles: "), paste(percentile[1:5], collapse = " "), "\n",
+      padleft(" "), paste(percentile[6:10], collapse = " "), sep = "")
 }
 
+codebook_header <- function(x, width) {
+  cat(rep('-', width), "\n", sep = "")
+
+  var_name <- deparse(substitute(x, env = parent.frame()))
+  var_label <- format(attr(x, "label"), w = width - nchar(var_name), justify = "right")
+  cat(var_name, var_label, "\n", sep = "")
+
+  cat(rep('-', width), "\n", sep = "")
+}
